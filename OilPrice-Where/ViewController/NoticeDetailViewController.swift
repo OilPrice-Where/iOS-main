ff74a96fbf2d7cc9cff789acfd3b6c14973c2c26
HimchanPark <ting1099@gmail.com>
2018-08-31 18:10:16 +0900

    - 공지사항 수정

--- OilPrice-Where/ViewController/NoticeDetailViewController.swift
+++ OilPrice-Where/ViewController/NoticeDetailViewController.swift
@@ -10,30 +10,40 @@ import UIKit
 
 class NoticeDetailViewController: UIViewController {
     
-    @IBOutlet weak var detailTitleLabel: UILabel!
-    @IBOutlet weak var contentTextView: UITextView!
     
-    var titleText: String = ""
+    @IBOutlet weak var contentLable: UILabel!
+    
+    let content = """
+    아직도 운전하다가 아무곳에서 기름을 넣으시나요?
+    
+    이제는 주변에서 가장 저렴한 주유소를 찾아보세요!
+    
+    어디주유앱이 도와드립니다.
+    
+    - 내가 간 주유소 가격이 저렴할까요?
+      전국 평균가와 비교해보세요! 바로 알 수 있답니다.
+
+    - 저렴한 주유소를 찾으셨나요?
+      카카오네비로 바로 길안내 해드려요.
+
+    - 자주가는 주유소는 즐겨찾기 해주세요!
+      직접가서 확인할 필요없이 바로 실시간으로 가격을 알려드립니다.
+    """
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        detailTitleLabel.textColor = UIColor.white
-        detailTitleLabel.text = titleText
-        print("**************************************\(titleText)")
-        
+        contentLable.text = content
+        contentLable.font = UIFont(name: "NanumSquareRoundEB", size: 17)
         navigationItem.leftBarButtonItem?.tintColor = UIColor.white
         navigationSetting()
-        contentTextView.layer.cornerRadius = 6
+        contentLable.layer.cornerRadius = 6
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        detailTitleLabel.text = titleText
-    }
     
-
+    
     func navigationSetting() {
         let backButtonItem = UIBarButtonItem()
         backButtonItem.title = ""
@@ -41,6 +51,6 @@ class NoticeDetailViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButtonItem
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont(name: "NanumSquareRoundEB", size: 18)!, NSAttributedStringKey.foregroundColor: UIColor.white]
     }
-
+    
     
 }
