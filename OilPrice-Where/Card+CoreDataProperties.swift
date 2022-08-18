b9eb14cf2bbed935201ce16eadad8fcb9c1d462f
wargi <wkahdla12346@gmail.com>
2022-08-18 23:36:31 +0900

    etc: 카드 모델 추가

--- OilPrice-Where/Card+CoreDataProperties.swift
+++ OilPrice-Where/Card+CoreDataProperties.swift
@@ -10,18 +10,18 @@
 import Foundation
 import CoreData
 
-
-extension Card {
-
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Card> {
-        return NSFetchRequest<Card>(entityName: "Card")
-    }
-
+@objc(Card)
+public class Card: NSManagedObject {
+    @NSManaged public var identifier: String
     @NSManaged public var name: String?
     @NSManaged public var isLiter: Bool
     @NSManaged public var saleValue: Double
     @NSManaged public var applyBrands: [String]?
-
+    @NSManaged public var insertDate: Date?
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Card> {
+        return NSFetchRequest<Card>(entityName: "Card")
+    }
 }
 
 extension Card : Identifiable {
