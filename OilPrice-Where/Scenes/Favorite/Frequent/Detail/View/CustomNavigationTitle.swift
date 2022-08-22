b9c6f56edb399402c123d78e5c155e7581733803
wargi <wkahdla12346@gmail.com>
2022-08-22 23:57:49 +0900

    etc: 공용 텍스트필드 추가

--- OilPrice-Where/Scenes/Favorite/Frequent/Detail/View/CustomNavigationTitle.swift
+++ OilPrice-Where/Scenes/Favorite/Frequent/Detail/View/CustomNavigationTitle.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2022 sangwook park. All rights reserved.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: CustomNavigationTitle
+final class CustomNavigationTitle: UIView {
+    //MARK: - Properties
+    let titleLabel = UILabel().then {
+        $0.textAlignment = .left
+        $0.font = FontFamily.NanumSquareRound.bold.font(size: 15)
+        $0.textColor = .white
+    }
+    let logoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not Created View")
+    }
+    
+    //MARK: - Make UI
+    func makeUI() {
+        addSubview(titleLabel)
+        addSubview(logoImageView)
+        
+        logoImageView.snp.makeConstraints {
+            $0.left.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(25)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.left.equalTo(logoImageView.snp.right).offset(4)
+            $0.centerY.right.equalToSuperview()
+        }
+    }
+}
