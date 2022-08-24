7d5053b3fb5cfd2f9017936c688a6d9749d25d6e
wargi <wkahdla12346@gmail.com>
2022-08-24 20:17:25 +0900

    etc: POI 데이터 모델 생성

--- OilPrice-Where/Scenes/Main/Search/Model/ResponsePOI.swift
+++ OilPrice-Where/Scenes/Main/Search/Model/ResponsePOI.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import CoreLocation
+
+struct ResponsePOI: Hashable {
+    var id = UUID()
+    let name: String?
+    let address: String?
+    let coordinate: CLLocationCoordinate2D?
+}
+
+extension ResponsePOI {
+    static func == (lhs: ResponsePOI, rhs: ResponsePOI) -> Bool {
+        return lhs.name == rhs.name &&
+                lhs.coordinate?.latitude == rhs.coordinate?.latitude &&
+                lhs.coordinate?.longitude == rhs.coordinate?.longitude &&
+                lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(coordinate?.latitude)
+        hasher.combine(coordinate?.longitude)
+    }
+}
--- OilPrice-Where/Scenes/Main/Search/View/SearchResultCell.swift
+++ OilPrice-Where/Scenes/Main/Search/View/SearchResultCell.swift
@@ -6,4 +6,64 @@
 //  Copyright © 2022 sangwook park. All rights reserved.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: SearchResultCell
+final class SearchResultCell: UITableViewCell {
+    //MARK: - Properties
+    let titleLabel = UILabel().then {
+        $0.textColor = .systemGray4
+        $0.font = FontFamily.NanumSquareRound.bold.font(size: 16)
+    }
+    let subTitleLabel = UILabel().then {
+        $0.textColor = .systemGray3
+        $0.font = FontFamily.NanumSquareRound.regular.font(size: 14)
+    }
+    let distanceLabel = UILabel().then {
+        $0.textAlignment = .right
+        $0.font = FontFamily.NanumSquareRound.regular.font(size: 14)
+    }
+    let lineView = UIView().then {
+        $0.backgroundColor = .systemGray5
+    }
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subTitleLabel)
+        contentView.addSubview(distanceLabel)
+        contentView.addSubview(lineView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview().inset(16)
+            $0.height.equalTo(26)
+        }
+        distanceLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.right.equalToSuperview().offset(-16)
+        }
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.left.equalToSuperview().inset(16)
+            $0.right.equalTo(distanceLabel.snp.left).offset(4)
+        }
+        lineView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+}
+
