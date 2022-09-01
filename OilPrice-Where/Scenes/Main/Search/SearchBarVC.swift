aa035ee4f424762b157fe73922424dc0646d8e60
wargi <wkahdla12346@gmail.com>
2022-09-01 22:01:30 +0900

    etc: SearchBarVC 기능 추가 및 수정

--- OilPrice-Where/Scenes/Main/Search/SearchBarVC.swift
+++ OilPrice-Where/Scenes/Main/Search/SearchBarVC.swift
@@ -14,7 +14,7 @@ import CoreLocation
 import CombineCocoa
 
 protocol SearchBarDelegate: AnyObject {
-    func fetch(coordinate: CLLocationCoordinate2D?)
+    func fetch(name: String?, coordinate: CLLocationCoordinate2D?)
 }
 
 //MARK: SearchBarVC
@@ -32,14 +32,25 @@ final class SearchBarVC: UIViewController {
         $0.image = Asset.Images.search.image.withRenderingMode(.alwaysTemplate)
         $0.tintColor = .systemGray5
     }
-    let searchBarView = CommonTextFieldView(titleWidth: 5, contentTrailing: 8.0).then {
+    let searchBarView = CommonTextFieldView(titleWidth: 0.1, contentTrailing: 8.0).then {
         $0.contentTextField.clearButtonMode = .always
         $0.contentTextField.attributedPlaceholder = NSAttributedString(string: "주유소 위치를 검색해보세요.", attributes: [
             .foregroundColor: UIColor.systemGray4,
             .font: FontFamily.NanumSquareRound.regular.font(size: 12)
         ])
     }
+    lazy var recentTableView = UITableView().then {
+        $0.delegate = self
+        $0.separatorStyle = .none
+        $0.separatorColor = .clear
+        $0.keyboardDismissMode = .onDrag
+        $0.alwaysBounceVertical = false
+        $0.alwaysBounceHorizontal = false
+        $0.showsHorizontalScrollIndicator = false
+        RecentResultCell.register($0)
+    }
     lazy var searchResultTableView = UITableView().then {
+        $0.isHidden = true
         $0.delegate = self
         $0.separatorStyle = .none
         $0.separatorColor = .clear
@@ -49,7 +60,9 @@ final class SearchBarVC: UIViewController {
         $0.showsHorizontalScrollIndicator = false
         SearchResultCell.register($0)
     }
-    var dataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
+    
+    var searchDataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
+    var recentDataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -63,6 +76,7 @@ final class SearchBarVC: UIViewController {
         super.viewWillAppear(animated)
         
         navigationController?.navigationBar.isHidden = true
+        searchBarView.contentTextField.becomeFirstResponder()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -78,6 +92,7 @@ final class SearchBarVC: UIViewController {
         view.addSubview(navigationView)
         view.addSubview(searchBarView)
         view.addSubview(searchImageView)
+        view.addSubview(recentTableView)
         view.addSubview(searchResultTableView)
         
         navigationView.snp.makeConstraints {
@@ -95,12 +110,18 @@ final class SearchBarVC: UIViewController {
             $0.left.equalTo(searchBarView.snp.left).offset(10)
             $0.centerY.equalTo(searchBarView)
         }
+        recentTableView.snp.makeConstraints {
+            $0.top.equalTo(searchBarView.snp.bottom).offset(8)
+            $0.left.bottom.right.equalToSuperview().inset(16)
+        }
         searchResultTableView.snp.makeConstraints {
             $0.top.equalTo(searchBarView.snp.bottom).offset(8)
-            $0.left.bottom.right.equalToSuperview()
+            $0.left.bottom.right.equalToSuperview().inset(16)
         }
         
-        performDataSource()
+        performRecentDataSource()
+        performSearchDataSource()
+        performRecentDataSnapShot()
     }
     
     //MARK: - Rx Binding..
@@ -130,6 +151,16 @@ final class SearchBarVC: UIViewController {
             .assign(to: \.tintColor, on: searchImageView)
             .store(in: &viewModel.bag)
         
+        searchBarView
+            .contentTextField
+            .textPublisher
+            .receive(on: DispatchQueue.main)
+            .compactMap { $0 }
+            .filter { $0.count == 0 }
+            .map { $0.isEmpty }
+            .assign(to: \.isHidden, on: searchResultTableView)
+            .store(in: &viewModel.bag)
+        
         viewModel
             .output
             .resultPOIs
@@ -139,7 +170,9 @@ final class SearchBarVC: UIViewController {
                     print(error.localizedDescription)
                 }
             } receiveValue: { [weak self] in
-                self?.performDataSnapShot(pois: $0)
+                self?.viewModel.pois = $0
+                self?.searchResultTableView.isHidden = $0.isEmpty
+                self?.performSearchDataSnapShot(pois: $0)
             }
             .store(in: &viewModel.bag)
         
@@ -160,8 +193,19 @@ final class SearchBarVC: UIViewController {
 
 //MARK:- DiffDataSource
 extension SearchBarVC: UITableViewDelegate {
-    private func performDataSource() {
-        dataSource = UITableViewDiffableDataSource<Section, ResponsePOI>(tableView: searchResultTableView, cellProvider: { [weak self] tableView, indexPath, poi in
+    private func performRecentDataSource() {
+        recentDataSource = UITableViewDiffableDataSource<Section, ResponsePOI>(tableView: recentTableView, cellProvider: { tableView, indexPath, poi in
+            let cell = tableView.dequeueReusableCell(withType: RecentResultCell.self, for: indexPath)
+            
+            cell.configure(with: poi, index: indexPath.row)
+            cell.delegate = self
+            
+            return cell
+        })
+    }
+    
+    private func performSearchDataSource() {
+        searchDataSource = UITableViewDiffableDataSource<Section, ResponsePOI>(tableView: searchResultTableView, cellProvider: { [weak self] tableView, indexPath, poi in
             let cell = tableView.dequeueReusableCell(withType: SearchResultCell.self, for: indexPath)
             
             let attrString = NSMutableAttributedString(string: poi.name ?? "")
@@ -183,20 +227,54 @@ extension SearchBarVC: UITableViewDelegate {
         })
     }
     
-    private func performDataSnapShot(pois: [ResponsePOI]) {
+    private func performRecentDataSnapShot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ResponsePOI>()
+        snapshot.appendSections([.search])
+        
+        let pois = DataManager.shared.pois.map {
+            ResponsePOI(name: $0.name,
+                        address: $0.address,
+                        coordinate: CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude),
+                        insertDate: $0.insertDate)
+        }
+        
+        snapshot.appendItems(pois)
+        self.recentDataSource?.apply(snapshot, animatingDifferences: false)
+    }
+    
+    private func performSearchDataSnapShot(pois: [ResponsePOI]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, ResponsePOI>()
         snapshot.appendSections([.search])
         
         snapshot.appendItems(pois)
-        self.dataSource?.apply(snapshot, animatingDifferences: false)
+        self.searchDataSource?.apply(snapshot, animatingDifferences: false)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 86
+        return tableView == recentTableView ? 50 : 86
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.fetch(coordinate: viewModel.pois[indexPath.row].coordinate)
+        if tableView != recentTableView {
+            DataManager.shared.addNew(poi: viewModel.pois[indexPath.row])
+            delegate?.fetch(name: viewModel.pois[indexPath.row].name, coordinate: viewModel.pois[indexPath.row].coordinate)
+        } else {
+            let target = CLLocationCoordinate2D(latitude: DataManager.shared.pois[indexPath.row].latitude,
+                                                longitude: DataManager.shared.pois[indexPath.row].longitude)
+            delegate?.fetch(name: DataManager.shared.pois[indexPath.row].name, coordinate: target)
+        }
+        
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension SearchBarVC: RecentResultCellProtocol {
+    func delete(poi: POI?, index: Int?) {
+        guard let poi, let index else { return }
+        
+        DataManager.shared.delete(value: poi)
+        DataManager.shared.pois.remove(at: index)
+        
+        performRecentDataSnapShot()
     }
 }
