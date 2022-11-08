49f05c3d3b5e38cc19505b4d2576bdd38a8dc002
wargi <wkahdla12346@gmail.com>
2022-11-08 21:05:47 +0900

    etc: live activies 추가

---
+++ OilPrice-Where/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SceneDelegate.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 2022/11/11.
+//  Copyright © 2022 sangwook park. All rights reserved.
+//
+
+import Foundation
---
+++ OilPrice-Where/StationAttributes.swift
@@ -0,0 +1,9 @@
+//
+//  StationAttributes.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 2022/11/10.
+//  Copyright © 2022 sangwook park. All rights reserved.
+//
+
+import Foundation
