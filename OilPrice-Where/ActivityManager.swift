bfad34684429d1a4fa16029d4b6e270787ea702f
wargi <wkahdla12346@gmail.com>
2022-11-09 23:10:17 +0900

    etc: activities 관련 모델 및 매니저 추가

--- OilPrice-Where/ActivityManager.swift
+++ OilPrice-Where/ActivityManager.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import ActivityKit
+
+@available(iOS 16.1, *)
+final class ActivityManager: NSObject {
+    // MARK: - Properties
+    static let shared = ActivityManager()
+    var activity: Activity<StationAttributes>? = nil
+    
+    // MARK: - Initializer
+    private override init() {
+        super.init()
+        
+        configure()
+    }
+    
+    deinit {
+        Task {
+            await endAllActivities()
+        }
+    }
+    
+    func configure() {
+        Task {
+            await endAllActivities()
+            activity = nil
+            let attributes = StationAttributes()
+            let state = StationAttributes.ContentState()
+            
+            activity = try? Activity<StationAttributes>.request(attributes: attributes, contentState: state, pushType: nil)
+        }
+    }
+    
+    func updateActivity(state: StationAttributes.ContentState) {
+        Task {
+            await activity?.update(using: state)
+        }
+    }
+    
+    func endAllActivities() async {
+        for act in Activity<StationAttributes>.activities {
+            await act.end(dismissalPolicy: .immediate)
+        }
+    }
+    
+    func station() -> FindStation? {
+        return activity?.contentState.station
+    }
+}
--- OilPrice-Where/StationAttributes.swift
+++ OilPrice-Where/StationAttributes.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2022 sangwook park. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ActivityKit
+
+struct FindStation: Codable, Hashable {
+    let id: String?
+    let name: String? // 매장 이름
+    let brand: String?
+    let oil: String?
+    let price: Int?
+    let lat: Double?
+    let lng: Double?
+    var distance: String?
+}
+
+struct StationAttributes: ActivityAttributes {
+    struct ContentState: Codable, Hashable {
+        var station: FindStation?
+    }
+}
