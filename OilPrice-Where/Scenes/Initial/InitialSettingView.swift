760f537d5d6f417078312315024b39100f7557d4
wargi <wkahdla12346@gmail.com>
2023-11-11 17:04:44 +0900

    feature: initialView 생성

---
+++ OilPrice-Where/Scenes/Initial/InitialSettingView.swift
@@ -0,0 +1,19 @@
+//
+//  InitialSettingView.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/7/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import SwiftUI
+
+struct InitialSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialSettingView()
+}
---
+++ OilPrice-Where/Scenes/Initial/View/InitialPickerView.swift
@@ -0,0 +1,19 @@
+//
+//  InitialPickerView.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/7/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import SwiftUI
+
+struct InitialPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialPickerView()
+}
