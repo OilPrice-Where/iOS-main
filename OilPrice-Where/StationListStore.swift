d9d246e0604e94e543cde17a5a089ecb3a6803a3
wargi <wkahdla12346@gmail.com>
2023-11-16 22:58:28 +0900

    refactoring: Station List 관련 파일 추가

---
+++ OilPrice-Where/StationListStore.swift
@@ -0,0 +1,25 @@
+//
+//  StationListStore.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/8/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import Foundation
+import ComposableArchitecture
+
+
+struct StationListReducer: Reducer {
+    struct State: Equatable {
+        
+    }
+    
+    enum Action: Equatable {
+        
+    }
+    
+    var body: some Reducer<State, Action> {
+        return .none
+    }
+}
---
+++ OilPrice-Where/StationRow.swift
@@ -0,0 +1,19 @@
+//
+//  StationRow.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/8/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import SwiftUI
+
+struct StationRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StationRow()
+}
