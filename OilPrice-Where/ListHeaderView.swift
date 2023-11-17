bd0795a8d0e8850fa27ce7d541dcadc9e9c0741b
wargi <wkahdla12346@gmail.com>
2023-11-17 20:34:11 +0900

    refactoring: Station List View header 추가

--- OilPrice-Where/ListHeaderView.swift
+++ OilPrice-Where/ListHeaderView.swift
@@ -7,13 +7,51 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
 
 struct ListHeaderView: View {
+    
+    let store: StoreOf<StationListReducer>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            HStack {
+                Button {
+                    viewStore.send(.orderByPrice)
+                } label: {
+                    Text("가격순")
+                        .font(viewStore.state.isOrderByPrice ? FontFamily.NanumSquareRound.extraBold.swiftUIFont(size: 16) : FontFamily.NanumSquareRound.regular.swiftUIFont(size: 16))
+                        .foregroundStyle(viewStore.state.isOrderByPrice ? Asset.Colors.mainColor.swiftUIColor : Asset.Colors.defaultColor.swiftUIColor)
+                }
+
+                Button {
+                    viewStore.send(.orderByDistance)
+                } label: {
+                    Text("거리순")
+                        .font(viewStore.state.isOrderByPrice ? FontFamily.NanumSquareRound.regular.swiftUIFont(size: 16) : FontFamily.NanumSquareRound.extraBold.swiftUIFont(size: 16))
+                        .foregroundStyle(viewStore.state.isOrderByPrice ? Asset.Colors.defaultColor.swiftUIColor : Asset.Colors.mainColor.swiftUIColor)
+                }
+                
+                Spacer()
+                
+                Asset.Images.geoIcon.swiftUIImage
+                    .renderingMode(.template)
+                    .resizable()
+                    .tint(.black)
+                    .frame(width: 16, height: 16)
+                    .padding(.trailing, -8)
+                
+                Text(viewStore.state.address)
+                    .font(FontFamily.NanumSquareRound.regular.swiftUIFont(size: 14))
+            }
+        }
     }
 }
 
 #Preview {
-    ListHeaderView()
+    ListHeaderView(store: Store(initialState: StationListReducer.State(),
+                                reducer: {
+       StationListReducer()
+   }))
 }
