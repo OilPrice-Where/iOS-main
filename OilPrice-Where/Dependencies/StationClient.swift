8c5ca7e584e27d5e302d93087230f49a5276810a
wargi <wkahdla12346@gmail.com>
2023-11-21 20:02:20 +0900

    refactoring: StationClient, Service 생성

---
+++ OilPrice-Where/Dependencies/StationClient.swift
@@ -0,0 +1,9 @@
+//
+//  StationClient.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/8/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import Foundation
---
+++ OilPrice-Where/StationService.swift
@@ -0,0 +1,9 @@
+//
+//  StationClientManager.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/8/24.
+//  Copyright © 2024 sangwook park. All rights reserved.
+//
+
+import Foundation
