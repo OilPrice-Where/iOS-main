038bab64c408cf2e91c482ccf62013b2dc993e84
wargi <wkahdla12346@gmail.com>
2023-11-29 20:55:41 +0900

    refactoring: 자식 Scope 연결

--- OilPrice-Where/MainStore.swift
+++ OilPrice-Where/MainStore.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2024 sangwook park. All rights reserved.
 //
 
+import ComposableArchitecture
 import Foundation
+
+
+@Reducer
+struct MainReducer {
+    struct State: Equatable {
+        var sideMenu = SideMenuReducer.State()
+        var search = StationSearchReducer.State()
+        var stationList = StationListReducer.State()
+        var favorite = FavoriteReducer.State()
+    }
+    
+    enum Action: BindableAction, Equatable {
+        case binding(BindingAction<State>)
+        case side(SideMenuReducer.Action)
+        case search(StationSearchReducer.Action)
+        case stationList(StationListReducer.Action)
+        case favorite(FavoriteReducer.Action)
+    }
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+        Scope(state: \.sideMenu, action: /Action.side) {
+            SideMenuReducer()
+        }
+        Scope(state: \.search, action: /Action.search) {
+            StationSearchReducer()
+        }
+        Scope(state: \.stationList, action: /Action.stationList) {
+            StationListReducer()
+        }
+        Scope(state: \.favorite, action: /Action.favorite) {
+            FavoriteReducer()
+        }
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                return .none
+            case .side:
+                return .none
+            case .search:
+                return .none
+            case .stationList:
+                return .none
+            case .favorite:
+                return .none
+            }
+        }
+    }
+}
