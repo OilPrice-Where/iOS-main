e973dfe8ef0dd9960d7569f0ea8326eabc961db2
wargi <wkahdla12346@gmail.com>
2024-12-01 19:02:44 +0900

    Feature: OilPriceEntity 추가

--- OilPrice-Where/Domain/Entities/OilPriceEntity.swift
+++ OilPrice-Where/Domain/Entities/OilPriceEntity.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+
+struct OilPriceEntity: Hashable, Codable {
+    /// 거래 일자
+    let tradeDate: String
+    /// 기름 코드
+    let oilCode: String
+    /// 유종 명
+    let oilName: String
+    /// 기름 타입
+    let oilType: String
+    /// 가격
+    let price: String
+    /// 저번주 평균가와 차이
+    let diff: String
+}
