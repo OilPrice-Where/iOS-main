c4e34aabfdb8a3465c9bed3aa9327924e7c2918c
wargi <wkahdla12346@gmail.com>
2024-12-02 20:00:55 +0900

    Feature: FirebaseUtil 대체를 위한 AverageCostRepository 추가

--- OilPrice-Where/Domain/Interfaces/Repositories/AverageCostRepository.swift
+++ OilPrice-Where/Domain/Interfaces/Repositories/AverageCostRepository.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+
+protocol AverageCostRepository {
+    /// 선택한 유가 정보
+    func fetchAverageCost(for productName: String) async throws -> NSDictionary
+    /// 평균 유가 데이터를 확인 후 업데이트
+    func checkAndUpdateAverageCosts()
+}
