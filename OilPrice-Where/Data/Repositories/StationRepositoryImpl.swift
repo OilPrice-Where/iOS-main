91a00e03adb2f3293ea4219f601226ef8a1e512d
wargi <wkahdla12346@gmail.com>
2024-12-04 20:03:31 +0900

    Feature: StationRepository 작성 및 implementation에서 필요한 구체 함수 작성

--- OilPrice-Where/Data/Repositories/StationRepositoryImpl.swift
+++ OilPrice-Where/Data/Repositories/StationRepositoryImpl.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Moya
+
+
+final class StationRepositoryImpl: StationRepository {
+    private let provider: MoyaProvider<StationAPI>
+    
+    init(provider: MoyaProvider<StationAPI> = MoyaProvider<StationAPI>()) {
+        self.provider = provider
+    }
+    
+    func fetchNearbyGasStations(x: Double, y: Double, radius: Int, prodcd: String, sort: Int, appKey: String) {
+        
+    }
+    
+    func fetchStationDetail(appKey: String, id: String) {
+        
+    }
+    
+    func fetchOilPriceResult(appKey: String) async throws -> [OilPriceEntity] {
+        try await withCheckedThrowingContinuation { continuation in
+            provider.request(.oilPriceResult(appKey: appKey)) { result in
+                switch result {
+                case .success(let response):
+                    do {
+                        let oilPriceResultDTO = try response.map(OilPriceResultDTO.self)
+                        continuation.resume(returning: oilPriceResultDTO.toDomain())
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
--- OilPrice-Where/Domain/Interfaces/Repositories/StationRepository.swift
+++ OilPrice-Where/Domain/Interfaces/Repositories/StationRepository.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+
+protocol StationRepository {
+    /// 주변 주유소 정보를 조회합니다.
+    func fetchNearbyGasStations(x: Double, y: Double, radius: Int, prodcd: String, sort: Int, appKey: String)
+    /// 특정 주유소의 상세 정보를 조회합니다.
+    func fetchStationDetail(appKey: String, id: String)
+    /// 유가 정보를 조회합니다.
+    func fetchOilPriceResult(appKey: String) async throws -> [OilPriceEntity]
+}
