237ba6837ab6de538d8eeeb2cb2e72ec78a8ce13
wargi <wkahdla12346@gmail.com>
2024-12-05 22:44:51 +0900

    Feature: Widget 및 불필요한 파일 삭제

--- OilPrice-Where/Application/AppDelegate.swift
+++ OilPrice-Where/Application/AppDelegate.swift
@@ -15,11 +15,12 @@ import KakaoSDKCommon
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    let firebaseUtility = FirebaseUtility()
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        firebaseUtility.checkUpdateTime()
+        
+        let stationRepository: StationRepository = StationRepositoryImpl()
+        let averageCostRepository: AverageCostRepository = FirebaseAverageCostRepository(repository: stationRepository)
+        averageCostRepository.checkAndUpdateAverageCosts()
         
         #if DEBUG
         var newArguments = ProcessInfo.processInfo.arguments
@@ -31,8 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         KakaoSDK.initSDK(appKey: "b8e7f9ac5bf3c19414515867205f92aa")
         DefaultData.shared.allPriceDataLoad() // 전국의 오일종류 별 저번주의 평균 값을 받아온다.
         
-        if #available(iOS 16.1, *),
-           DefaultData.shared.backgroundFindSubject.value {
+        if DefaultData.shared.backgroundFindSubject.value {
             ActivityManager.shared.configure()
         }
         
