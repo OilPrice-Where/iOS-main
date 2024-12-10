301fd2edd0fc715b799769f3ddfde6f3f163c975
wargi <wkahdla12346@gmail.com>
2024-12-10 20:15:44 +0900

    Feature: DefaultData 관리포인트 없애기 위한 SettingUseCase 추가

--- OilPrice-Where/Domain/UseCases/SettingUseCase.swift
+++ OilPrice-Where/Domain/UseCases/SettingUseCase.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+
+protocol SettingUseCase {
+    func load<T>(type: SettingType) throws -> T
+    func save<T>(_ value: T, type: SettingType)
+}
+
+
+final class SettingUseCaseImpl: SettingUseCase {
+    
+    private let storage: SettingStorage
+    
+    init(storage: SettingStorage) {
+        self.storage = storage
+    }
+    
+    func load<T>(type: SettingType) throws -> T {
+        guard let defaultValue = type.defaultValue as? T else {
+            throw SettingError.invalidDefaultValue
+        }
+        
+        return storage.fetchValue(
+            defaultValue: defaultValue,
+            forKey: type.key,
+            fromStorageNamed: Constants.storageName
+        )
+    }
+    
+    func save<T>(_ value: T, type: SettingType) {
+        storage.save(value, forKey: type.key, to: Constants.storageName)
+    }
+}
+
+private extension SettingUseCaseImpl {
+    enum Constants {
+        static let storageName: String = "UserInfo"
+    }
+}
