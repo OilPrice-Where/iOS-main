2c142ca1d4e4eb7140ae0ddc8f3cbd0466d111a4
wargi <wkahdla12346@gmail.com>
2024-12-15 22:08:22 +0900

    Feature: VM 관련 로직 분리된 부분 수정

--- OilPrice-Where/Scenes/Menu/AboutUs/SettingAboutUsVC.swift
+++ OilPrice-Where/Scenes/Menu/AboutUs/SettingAboutUsVC.swift
@@ -7,100 +7,121 @@
 //
 
 import UIKit
-import SnapKit
+import Combine
 import Then
+import SnapKit
+
+
 //MARK: SettingAboutUsVC
 final class SettingAboutUsVC: CommonViewController {
-    enum SelectGitCell: Int {
-        case wargi = 0
-        case himchan = 1
-        case solchan = 2
-    }
     
     //MARK: - Properties
-    private lazy var tableView = UITableView().then {
-        $0.dataSource = self
-        $0.delegate = self
-        $0.alwaysBounceVertical = false
-        $0.alwaysBounceHorizontal = false
-        $0.showsVerticalScrollIndicator = false
-        $0.showsHorizontalScrollIndicator = false
-        $0.backgroundColor = Asset.Colors.tableViewBackground.color
-        AboutUsTableViewCell.register($0)
-    }
+    let viewModel: SettingAboutUsViewModel
+    
+    private var collectionView: UICollectionView!
+    private var dataSource: SettingAboutUsDataSource!
+    
+    private let didSelectItemPublisher = PassthroughSubject<SettingAboutUsViewModel.AboutMe, Never>()
     
     //MARK: - Life Cycle
+    init(viewModel: SettingAboutUsViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         makeUI()
+        bindUI()
+        bindActions()
+    }
+}
+
+private extension SettingAboutUsVC {
+    func bindUI() {
+        viewModel.aboutUsPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] aboutUs in
+                self?.dataSource.applySnapshot(with: aboutUs)
+            }
+            .store(in: &cancellable)
     }
     
-    //MARK: - Set UI
-    private func makeUI() {
+    func bindActions() {
+        let output = viewModel.transform(input: .init(
+            didSelectItem: didSelectItemPublisher.eraseToAnyPublisher()
+        ))
+        
+        output.openLink
+            .receive(on: DispatchQueue.main)
+            .sink { targetUrl in
+                guard UIApplication.shared.canOpenURL(targetUrl) else {
+                    return
+                }
+                UIApplication.shared.open(targetUrl, options: [:], completionHandler: nil)
+            }
+            .store(in: &cancellable)
+    }
+}
+
+
+//MARK: - Set UI
+private extension SettingAboutUsVC {
+    final class SettingAboutUsDataSource: UICollectionViewDiffableDataSource<CommonDiffableSection, SettingAboutUsViewModel.AboutMe> {
+        private typealias Snapshot = NSDiffableDataSourceSnapshot<CommonDiffableSection, SettingAboutUsViewModel.AboutMe>
+        
+        func applySnapshot(with items: [SettingAboutUsViewModel.AboutMe],
+                           animatingDifferences: Bool = true) {
+            var snapshot: Snapshot = .init()
+            snapshot.appendItems(items, toSection: .main)
+            apply(snapshot, animatingDifferences: animatingDifferences)
+        }
+    }
+    
+    func makeUI() {
         navigationItem.title = "About Us"
         
-        view.addSubview(tableView)
+        configureCollectionView()
+        configureDataSource()
+    }
+    
+    func configureCollectionView() {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
+            $0.delegate = self
+            $0.alwaysBounceVertical = false
+            $0.alwaysBounceHorizontal = false
+            $0.showsVerticalScrollIndicator = false
+            $0.showsHorizontalScrollIndicator = false
+            $0.backgroundColor = Asset.Colors.tableViewBackground.color
+        }
         
-        tableView.snp.makeConstraints {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
-}
-
-extension SettingAboutUsVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let selectCellType = SelectGitCell(rawValue: indexPath.row) else { return UITableViewCell() }
-        let cell = tableView.dequeueReusableCell(withType: AboutUsTableViewCell.self, for: indexPath)
-        
-        cell.selectionStyle = .none
-        var target: (name: String?, link: String?) = (nil, nil)
-        
-        switch selectCellType {
-        case .wargi:
-            target.name = "Wargi"
-            target.link = "github.com/wargi"
-        case .himchan:
-            target.name = "Himchan park"
-            target.link = "github.com/himchanPark"
-        case .solchan:
-            target.name = "Solchan ahn"
-            target.link = "github.com/solchan87"
+    func configureDataSource() {
+        self.dataSource = SettingAboutUsDataSource(collectionView: collectionView) { collectionView, indexPath, aboutMe in
+            collectionView.dequeueConfiguredReusableCell(using: AboutUsCell.cellRegistration, for: indexPath, item: aboutMe)
         }
-        
-        cell.configure(name: target.name, link: target.link)
-        
-        return cell
     }
 }
 
-extension SettingAboutUsVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 117
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let selectCellType = SelectGitCell(rawValue: indexPath.row) else { return }
-        
-        var targetUrlString = ""
-        
-        switch selectCellType {
-        case .wargi:
-            targetUrlString = "https://www.github.com/wargi"
-        case .himchan:
-            targetUrlString = "https://www.github.com/himchanPark"
-        case .solchan:
-            targetUrlString = "https://www.github.com/solchan87"
+extension SettingAboutUsVC: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let aboutMe = dataSource.itemIdentifier(for: indexPath) else {
+            return
         }
         
-        if let url = URL(string: targetUrlString) {
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-        }
+        didSelectItemPublisher.send(aboutMe)
     }
 }
