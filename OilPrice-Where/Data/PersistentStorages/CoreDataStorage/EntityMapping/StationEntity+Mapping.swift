3444fd91d519d316fb4d3fcfdea6b1cb0595eed6
wargi <wkahdla12346@gmail.com>
2024-12-17 20:09:11 +0900

    ETC: 이동이 적은 폴더 Buildable Folder로 변경

---
+++ OilPrice-Where/Data/PersistentStorages/CoreDataStorage/EntityMapping/StationEntity+Mapping.swift
@@ -0,0 +1,15 @@
+//
+//  CardEntity+Mapping.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/21/25.
+//  Copyright © 2025 sangwook park. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension CardEntity {
+    
+}
---
+++ OilPrice-Where/Data/PersistentStorages/CoreDataStorage/VisitedStationStorage.swift
@@ -0,0 +1,9 @@
+//
+//  VisitedStationStorage.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 1/21/25.
+//  Copyright © 2025 sangwook park. All rights reserved.
+//
+
+import Foundation
