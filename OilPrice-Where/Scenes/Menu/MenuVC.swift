da14fc6c3976374f741cac9a24109076ab94bc0a
wargi <wkahdla12346@gmail.com>
2024-12-27 23:01:03 +0900

    Feature: MenuViewModel 수정된 로직 VC 대응

--- OilPrice-Where/Scenes/Menu/MenuVC.swift
+++ OilPrice-Where/Scenes/Menu/MenuVC.swift
@@ -6,18 +6,15 @@
 //  Copyright © 2022 sangwook park. All rights reserved.
 //
 
-import Then
-import SnapKit
 import UIKit
 import Combine
-import Firebase
-import Toast
+import Then
+import SnapKit
 
-//MARK: MenuVC
+
 final class MenuVC: CommonViewController {
     //MARK: - Properties
-    var ref: DatabaseReference?
-    let viewModel = MenuViewModel()
+    let viewModel: MenuViewModel
     
     private let navigationView = MenuKeyValueView(title: "내비게이션", menuType: .keyValue)
     private let oilTypeView = MenuKeyValueView(title: "유종", menuType: .keyValue)
@@ -31,12 +28,22 @@ final class MenuVC: CommonViewController {
     
     
     //MARK: - Life Cycle
+    init(viewModel: MenuViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         makeUI()
-        bind()
-        configure()
+        bindUI()
+        bindActions()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,60 +51,12 @@ final class MenuVC: CommonViewController {
         
         navigationController?.navigationBar.isHidden = true
     }
-    
-    //MARK: - Make UI
-    func makeUI() {
-        view.backgroundColor = .white
-        view.addSubview(oilTypeView)
-        view.addSubview(navigationView)
-        view.addSubview(historyView)
-        view.addSubview(findBrandView)
-        view.addSubview(avgView)
-//        view.addSubview(cardSaleView)
-        view.addSubview(aboutView)
-        view.addSubview(reviewView)
-        view.addSubview(versionView)
-        
-        oilTypeView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(44)
-            $0.left.right.equalToSuperview()
-        }
-        navigationView.snp.makeConstraints {
-            $0.top.equalTo(oilTypeView.snp.bottom).offset(18)
-            $0.left.right.equalToSuperview()
-        }
-        historyView.snp.makeConstraints {
-            $0.top.equalTo(navigationView.snp.bottom).offset(40)
-            $0.left.right.equalToSuperview()
-        }
-        findBrandView.snp.makeConstraints {
-            $0.top.equalTo(historyView.snp.bottom).offset(18)
-            $0.left.right.equalToSuperview()
-        }
-        avgView.snp.makeConstraints {
-            $0.top.equalTo(findBrandView.snp.bottom).offset(18)
-            $0.left.right.equalToSuperview()
-        }
-//        cardSaleView.snp.makeConstraints {
-//            $0.top.equalTo(avgView.snp.bottom).offset(40)
-//            $0.left.right.equalToSuperview()
-//        }
-        versionView.snp.makeConstraints {
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-40)
-            $0.left.right.equalToSuperview()
-        }
-        reviewView.snp.makeConstraints {
-            $0.bottom.equalTo(versionView.snp.top)
-            $0.left.right.equalToSuperview()
-        }
-        aboutView.snp.makeConstraints {
-            $0.bottom.equalTo(reviewView.snp.top)
-            $0.left.right.equalToSuperview()
-        }
-    }
-    
-    //MARK: - Rx Binding..
-    func bind() {
+}
+
+
+//MARK: - Binding..
+private extension MenuVC {
+    func bindUI() {
         DefaultData.shared.naviSubject
             .map { Preferences.navigation(type: $0) }
             .assign(to: \.text, on: navigationView.valueLabel)
@@ -107,166 +66,199 @@ final class MenuVC: CommonViewController {
             .map { Preferences.oil(code: $0) }
             .assign(to: \.text, on: oilTypeView.valueLabel)
             .store(in: &viewModel.cancellable)
-                
-        // 내비게이션
-        navigationView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let vc = SelectMenuVC(type: .navigation)
-                vc.modalPresentationStyle = .overFullScreen
-                owner.present(vc, animated: false)
-            }
-            .store(in: &viewModel.cancellable)
+    }
+    
+    func bindActions() {
+        let selectedMenu: AnyPublisher<MenuViewModel.MenuType, Never> = Publishers.MergeMany(
+            navigationView.gesturePublisher().map { _ in .navigation }.eraseToAnyPublisher(),
+            oilTypeView.gesturePublisher().map { _ in .fuelType }.eraseToAnyPublisher(),
+            historyView.gesturePublisher().map { _ in .history }.eraseToAnyPublisher(),
+            avgView.gesturePublisher().map { _ in .priceAverage }.eraseToAnyPublisher(),
+            findBrandView.gesturePublisher().map { _ in .searchBrand }.eraseToAnyPublisher(),
+            cardSaleView.gesturePublisher().map { _ in .cardSale }.eraseToAnyPublisher(),
+            aboutView.gesturePublisher().map { _ in .aboutUs }.eraseToAnyPublisher(),
+            reviewView.gesturePublisher().map { _ in .editReview }.eraseToAnyPublisher(),
+            versionView.gesturePublisher().map { _ in .checkVersion }.eraseToAnyPublisher()
+        ).eraseToAnyPublisher()
         
-        // 유종
-        oilTypeView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let vc = SelectMenuVC(type: .oilType)
-                vc.modalPresentationStyle = .overFullScreen
-                owner.present(vc, animated: false)
-            }
-            .store(in: &viewModel.cancellable)
+        let output = viewModel.transform(input: .init(
+            selectedMenu: selectedMenu
+        ))
         
-        // 방문 내역
-        historyView
-            .gesturePublisher()
+        // 메뉴 VC 이동
+        output.presentViewController
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let navi = owner.viewModel.output.fetchNavigationController(type: .history)
-                owner.present(navi, animated: true)
+            .sink { [weak self] present in
+                self?.present(present.viewController, animated: present.animated)
             }
-            .store(in: &viewModel.cancellable)
+            .store(in: &cancellable)
         
-        // 전국 평균가
-        avgView
-            .gesturePublisher()
+        // 앱 리뷰 작성
+        output.openReview
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let self else { return }
-                
-                let stationRepository: StationRepository = StationRepositoryImpl()
-                let averageCostRepository: AverageCostRepository = FirebaseAverageCostRepository(stationRepository: stationRepository)
-                let averageCostUseCase: AverageCostUseCase = AverageCostUseCaseImpl(averageCostRepository: averageCostRepository)
-                let priceAverageViewModel = PriceAverageViewModel(averageCostUseCase: averageCostUseCase)
-                let priceAverageVC = PriceAverageVC(viewModel: priceAverageViewModel)
-                priceAverageVC.modalPresentationStyle = .overFullScreen
-                present(priceAverageVC, animated: false)
-            }
-            .store(in: &viewModel.cancellable)
-        
-        // 검색 브랜드
-        findBrandView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let navi = owner.viewModel.output.fetchNavigationController(type: .findBrand)
-                owner.present(navi, animated: true)
-            }
-            .store(in: &viewModel.cancellable)
-        
-        // 카드 할인
-        cardSaleView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let navi = owner.viewModel.output.fetchNavigationController(type: .cardSale)
-                owner.present(navi, animated: true)
-            }
-            .store(in: &viewModel.cancellable)
-        
-        // AboutUs
-        aboutView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                let navi = owner.viewModel.output.fetchNavigationController(type: .aboutUs)
-                owner.present(navi, animated: true)
-            }
-            .store(in: &viewModel.cancellable)
-        
-        // 리뷰 작성
-        reviewView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self else { return }
-                owner.viewModel.output.fetchReview()
-            }
-            .store(in: &viewModel.cancellable)
-        
-        // 버전 확인
-        versionView
-            .gesturePublisher()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
-                guard let owner = self, let infoDic = Bundle.main.infoDictionary,
-                      let currentVersion = infoDic["CFBundleShortVersionString"] as? String else {
-                    let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-                    let alert = UIAlertController(title: "현재 사용 중인 버전", message: "최신 버전: \(currentVersion ?? "")", preferredStyle: .alert)
-                    
-                    let okAction = UIAlertAction(title: "확인", style: .default)
-                    alert.addAction(okAction)
-                    
-                    self?.present(alert, animated: true)
-                    
+            .sink { reviewURL in
+                // 유효한 URL인지 검사
+                guard UIApplication.shared.canOpenURL(reviewURL) else {
                     return
                 }
-                
-                owner.versionCheck { lastestVersion in
-                    if currentVersion != lastestVersion {
-                        let alert = UIAlertController(title: "최신 버전이 있습니다.", message: "설치된 버전: \(currentVersion)\n최신 버전: \(lastestVersion)", preferredStyle: .alert)
-                        let updateAction = UIAlertAction(title: "업데이트", style: .default) { _ in
-                            let id = "1435350344"
-                            if let appURL = URL(string: "itms-apps://itunes.apple.com/app/itunes-u/id\(id)"),
-                               UIApplication.shared.canOpenURL(appURL) {
-                                // 유효한 URL인지 검사
-                                if #available(iOS 10.0, *) { //iOS 10.0부터 URL를 오픈하는 방법이 변경 되었습니다.
-                                    UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
-                                } else {
-                                    UIApplication.shared.openURL(appURL)
-                                }
-                            }
-                        }
-                        let okAction = UIAlertAction(title: "확인", style: .default)
-                        
-                        alert.addAction(okAction)
-                        alert.addAction(updateAction)
-                        owner.present(alert, animated: true)
-                    } else {
-                        let alert = UIAlertController(title: "최신 버전을 사용 중입니다.", message: "설치된 버전: \(currentVersion)\n최신 버전: \(lastestVersion)", preferredStyle: .alert)
-                        
-                        let okAction = UIAlertAction(title: "확인", style: .default)
-                        alert.addAction(okAction)
-                        
-                        owner.present(alert, animated: true)
-                    }
-                }
+                UIApplication.shared.open(reviewURL, options: [:], completionHandler: nil)
             }
-            .store(in: &viewModel.cancellable)
+            .store(in: &cancellable)
+        
+        // 버전 확인
+        output.showVersionStatus
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] versionStatus in
+                self?.showVersionCheckAlertViewController(versionStatus: versionStatus)
+            }
+            .store(in: &cancellable)
     }
     
-    func configure() {
-        ref = Database.database().reference()
-    }
-    
-    func versionCheck(completion: @escaping (String) -> ()) {
-        guard let _ref = ref else { return }
+    func showVersionCheckAlertViewController(versionStatus: AppUpdateStatus) {
+        let alert: UIAlertController
         
-        let data = _ref.child("version").child("lastest_version_name")
-        
-        data.observeSingleEvent(of: .value) { snapshot in
-            guard let lastest_version_name = snapshot.value as? String else { return }
+        switch versionStatus {
+        case .upToDate(let title, let message):
+            alert = UIAlertController.createAlertContoller(
+                title: title,
+                message: message
+            )
             
-            completion(lastest_version_name)
+        case .optionalUpdate(let title, let message, let appURLString):
+            let nextUpdateAction = UIAlertAction(title: "나중에 하기", style: .destructive)
+            let updateAction = UIAlertAction(title: "지금 업데이트 하기", style: .default) { _ in
+                guard let appURL = URL(string: appURLString, encodingInvalidCharacters: false),
+                      UIApplication.shared.canOpenURL(appURL) else {
+                    return
+                }
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+            }
+            
+            alert = UIAlertController.createAlertContoller(
+                title: title,
+                message: message,
+                actions: [nextUpdateAction, updateAction]
+            )
+            
+        case .forcedUpdate(let title, let message, let appURLString):
+            let updateAction = UIAlertAction(title: "업데이트 하기", style: .default) { _ in
+                guard let appURL = URL(string: appURLString, encodingInvalidCharacters: false),
+                      UIApplication.shared.canOpenURL(appURL) else {
+                    return
+                }
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+            }
+            
+            alert = UIAlertController.createAlertContoller(
+                title: title,
+                message: message,
+                actions: [updateAction]
+            )
+            
+        case .versionInfoUnavailable(let title, let message):
+            alert = UIAlertController.createAlertContoller(
+                title: title,
+                message: message
+            )
+        }
+        
+        present(alert, animated: true)
+    }
+}
+
+
+//MARK: - Set UI
+private extension MenuVC {
+    enum UIConstants {
+        enum OilTypeView {
+            static let topOffset: CGFloat = 44
+        }
+        
+        enum NavigationView {
+            static let topOffset: CGFloat = 18
+        }
+        
+        enum HistoryView {
+            static let topOffset: CGFloat = 40
+        }
+        
+        enum FindBrandView {
+            static let topOffset: CGFloat = 18
+        }
+        
+        enum AvgView {
+            static let topOffset: CGFloat = 18
+        }
+        
+        enum VersionView {
+            static let bottomOffset: CGFloat = -40
+        }
+    }
+
+    
+    func makeUI() {
+        view.backgroundColor = .white
+        
+        configureUI()
+        setConstraints()
+    }
+    
+    func configureUI() {
+        view.addSubview(oilTypeView)
+        view.addSubview(navigationView)
+        view.addSubview(historyView)
+        view.addSubview(findBrandView)
+        view.addSubview(avgView)
+//        view.addSubview(cardSaleView)
+        view.addSubview(aboutView)
+        view.addSubview(reviewView)
+        view.addSubview(versionView)
+    }
+    
+    func setConstraints() {
+        oilTypeView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(UIConstants.OilTypeView.topOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(oilTypeView.snp.bottom).offset(UIConstants.NavigationView.topOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+        historyView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(UIConstants.HistoryView.topOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+        findBrandView.snp.makeConstraints {
+            $0.top.equalTo(historyView.snp.bottom).offset(UIConstants.FindBrandView.topOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+        avgView.snp.makeConstraints {
+            $0.top.equalTo(findBrandView.snp.bottom).offset(UIConstants.AvgView.topOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+//        cardSaleView.snp.makeConstraints {
+//            $0.top.equalTo(avgView.snp.bottom).offset(40)
+//            $0.left.right.equalToSuperview()
+//        }
+        
+        versionView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(UIConstants.VersionView.bottomOffset)
+            $0.left.right.equalToSuperview()
+        }
+        
+        reviewView.snp.makeConstraints {
+            $0.bottom.equalTo(versionView.snp.top)
+            $0.left.right.equalToSuperview()
+        }
+        
+        aboutView.snp.makeConstraints {
+            $0.bottom.equalTo(reviewView.snp.top)
+            $0.left.right.equalToSuperview()
         }
     }
 }
