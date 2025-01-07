4abbf7dfb1051ba307c8b203789394fe397d8a4c
wargi <wkahdla12346@gmail.com>
2025-01-07 19:55:08 +0900

    Feature: InitialSettingsViewModel 네이밍 수정 및 관련 Model로 변경

---
+++ OilPrice-Where/Scenes/Initial/InitialSettingsViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  InitialSettingsViewModel.swift
+//  OilPrice-Where
+//
+//  Created by 박상욱 on 2020/08/03.
+//  Copyright © 2020 sangwook park. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+
+final class InitialSettingsViewModel {
+    //MARK: Properties
+    let settingUseCase: SettingUseCase
+    
+    init(settingUseCase: SettingUseCase) {
+        self.settingUseCase = settingUseCase
+    }
+}
+
+extension InitialSettingsViewModel {
+    struct SelectionResult {
+        var fuel: FuelType
+        var navigation: SearchNavigation
+    }
+    
+    struct Input {
+        /// 모든 설정이 선택 완료 이벤트
+        var allSettingsSelectedPublisher: AnyPublisher<SelectionResult, Never>
+    }
+    
+    struct Output {
+        /// 메인화면 이동
+        var moveMain: AnyPublisher<Void, Never>
+    }
+    
+    func transform(input: Input) -> Output {
+        let moveMain = input.allSettingsSelectedPublisher
+            .map { [weak self] selection in
+                guard let self else {
+                    return
+                }
+                settingUseCase.save(selection.fuel.code, type: .fuelType)
+                settingUseCase.save(selection.navigation.type, type: .navigationType)
+            }
+            .eraseToAnyPublisher()
+        
+        return .init(moveMain: moveMain)
+    }
+}
