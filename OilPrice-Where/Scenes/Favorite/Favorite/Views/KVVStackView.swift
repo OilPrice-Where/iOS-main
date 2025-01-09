234788e45b5012349d83f68c3581674b7451f872
박상욱(sangwookpark) <sangwookpark@gmarket.com>
2025-01-09 22:53:36 +0900

    Feature: KVVStackView UIConstants 분리

--- OilPrice-Where/Scenes/Favorite/Favorite/Views/KVVStackView.swift
+++ OilPrice-Where/Scenes/Favorite/Favorite/Views/KVVStackView.swift
@@ -7,20 +7,23 @@
 //
 
 import UIKit
-import SnapKit
 import Then
+import SnapKit
+
+
 //MARK: Key(Label)/Value(ImageView) VStack View
 final class KVVStackView: UIStackView {
-    // Properties
+    //MARK: Properties
     let keyLabel = UILabel().then {
         $0.textAlignment = .center
-        $0.font = FontFamily.NanumSquareRound.regular.font(size: 10)
+        $0.font = UIConstants.KeyLabel.font
     }
     let valueImageView = UIImageView().then {
         $0.contentMode = .scaleAspectFit
     }
     
-    // Initializer
+    
+    //MARK: Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -30,23 +33,48 @@ final class KVVStackView: UIStackView {
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+
+//MARK: - Set UI
+private extension KVVStackView {
+    enum UIConstants {
+        enum ContentView {
+            static let spacing: CGFloat = 3
+        }
+        
+        enum KeyLabel {
+            static let font: UIFont = FontFamily.NanumSquareRound.regular.font(size: 10)
+            
+            static let height: CGFloat = 30
+        }
+        
+        enum ValueImageView {
+            static let height: CGFloat = 14
+        }
+    }
     
-    // Set UI
-    private func makeUI() {
+    func makeUI() {
+        configureUI()
+        setConstraints()
+    }
+    
+    func configureUI() {
         axis = .vertical
         distribution = .fill
         alignment = .fill
-        spacing = 3
+        spacing = UIConstants.ContentView.spacing
         
         addArrangedSubview(valueImageView)
         addArrangedSubview(keyLabel)
-        
+    }
+    
+    func setConstraints() {
         valueImageView.snp.makeConstraints {
-            $0.height.equalTo(30)
+            $0.height.equalTo(UIConstants.KeyLabel.height)
         }
-        
         keyLabel.snp.makeConstraints {
-            $0.height.equalTo(14)
+            $0.height.equalTo(UIConstants.ValueImageView.height)
         }
     }
 }
