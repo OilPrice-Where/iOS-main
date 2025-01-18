ed579979d22c2ef1ca7d82a7cb48bb7863812fd7
wargi <wkahdla12346@gmail.com>
2025-01-18 23:54:32 +0900

    Feature: SearchVC View 분리 완료

--- OilPrice-Where/Scenes/Main/Search/SearchBarVC.swift
+++ OilPrice-Where/Scenes/Main/Search/SearchBarVC.swift
@@ -6,96 +6,76 @@
 //  Copyright © 2022 sangwook park. All rights reserved.
 //
 
-import Then
-import SnapKit
 import UIKit
 import Combine
-import CoreLocation
+import Then
+import SnapKit
+
 
 protocol SearchBarDelegate: AnyObject {
     func fetch(name: String?, coordinate: CLLocationCoordinate2D?)
 }
 
+
 //MARK: SearchBarVC
 final class SearchBarVC: CommonViewController {
     //MARK: - Properties
-    enum Section {
-        case search
-    }
-    
-    var bag = Set<AnyCancellable>()
     weak var delegate: SearchBarDelegate?
-    private let viewModel = SearchBarViewModel()
     
+    private let viewModel: SearchBarViewModel
     
-    let navigationView = CommonNavigationView().then {
-        $0.titleLabel.text = "주소 검색"
+    private let recentResultView = RecentResultView()
+    private let searchResultView = SearchResultView()
+    private let navigationView = CommonNavigationView().then {
+        $0.titleLabel.text = UIConstants.NavigationView.title
     }
-    
-    let searchImageView = UIImageView().then {
-        $0.image = Asset.Images.search.image.withRenderingMode(.alwaysTemplate)
+    private let searchImageView = UIImageView().then {
+        $0.image = UIConstants.SearchImageView.image
         $0.tintColor = .systemGray5
     }
-    
-    let searchBarView = CommonTextFieldView(titleWidth: 0.1, contentTrailing: 8.0).then {
+    private let searchBarView = CommonTextFieldView(
+        titleWidth: UIConstants.SearchBarView.titleWidth,
+        contentTrailing: UIConstants.SearchBarView.contentTrailing
+    ).then {
         $0.contentTextField.clearButtonMode = .always
-        $0.contentTextField.attributedPlaceholder = NSAttributedString(string: "주유소 위치를 검색해보세요.", attributes: [
-            .foregroundColor: UIColor.systemGray4,
-            .font: FontFamily.NanumSquareRound.regular.font(size: 12)
-        ])
+        $0.contentTextField.attributedPlaceholder = NSAttributedString(
+            string: UIConstants.SearchBarView.content,
+            attributes: [
+                .foregroundColor: UIColor.systemGray4,
+                .font: UIConstants.SearchBarView.contentFont
+            ]
+        )
     }
-    
-    let titleLabel = UILabel().then {
-        $0.text = "최근 검색"
-        $0.font = FontFamily.NanumSquareRound.bold.font(size: 14)
+    private let titleLabel = UILabel().then {
+        $0.text = UIConstants.TitleLabel.text
+        $0.font = UIConstants.TitleLabel.font
     }
-    
-    let removeAllButton = UIButton().then {
-        $0.setTitle("전체 삭제", for: .normal)
-        $0.setTitle("전체 삭제", for: .highlighted)
+    private let removeAllButton = UIButton().then {
+        $0.setTitle(UIConstants.RemoveAllButton.title, for: .normal)
         $0.setTitleColor(.systemGray, for: .normal)
-        $0.setTitleColor(.systemGray, for: .highlighted)
-        $0.titleLabel?.font = FontFamily.NanumSquareRound.regular.font(size: 12)
+        $0.titleLabel?.font = UIConstants.RemoveAllButton.font
     }
-    
-    lazy var recentTableView = UITableView().then {
-        $0.delegate = self
-        $0.separatorStyle = .none
-        $0.separatorColor = .clear
-        $0.keyboardDismissMode = .onDrag
-        $0.alwaysBounceVertical = false
-        $0.alwaysBounceHorizontal = false
-        $0.showsHorizontalScrollIndicator = false
-        RecentResultCell.register($0)
-    }
-    
-    lazy var searchResultTableView = UITableView().then {
-        $0.isHidden = true
-        $0.delegate = self
-        $0.separatorStyle = .none
-        $0.separatorColor = .clear
-        $0.keyboardDismissMode = .onDrag
-        $0.alwaysBounceVertical = false
-        $0.alwaysBounceHorizontal = false
-        $0.showsHorizontalScrollIndicator = false
-        SearchResultCell.register($0)
-    }
-    let emptyRecentSearchLabel = UILabel().then {
-        $0.text = "최근 검색한 내역이 없습니다"
+    private let emptyRecentSearchLabel = UILabel().then {
+        $0.text = UIConstants.EmptyRecentSearchLabel.text
         $0.textColor = .systemGray
         $0.textAlignment = .center
-        $0.font = FontFamily.NanumSquareRound.bold.font(size: 18)
-        $0.isHidden = true
+        $0.font = UIConstants.EmptyRecentSearchLabel.font
     }
     
-    var searchDataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
-    var recentDataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
     
     //MARK: - Life Cycle
+    init(viewModel: SearchBarViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         makeUI()
+        configureSearchResultView()
         bind()
     }
     
@@ -110,59 +90,6 @@ final class SearchBarVC: CommonViewController {
         super.touchesBegan(touches, with: event)
         
         view.endEditing(true)
-    }
-    
-    //MARK: - Make UI
-    private func makeUI() {
-        view.backgroundColor = .white
-        
-        view.addSubview(navigationView)
-        view.addSubview(searchBarView)
-        view.addSubview(searchImageView)
-        view.addSubview(titleLabel)
-        view.addSubview(removeAllButton)
-        view.addSubview(recentTableView)
-        view.addSubview(emptyRecentSearchLabel)
-        view.addSubview(searchResultTableView)
-        
-        navigationView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.left.right.equalToSuperview()
-            $0.height.equalTo(47)
-        }
-        searchBarView.snp.makeConstraints {
-            $0.top.equalTo(navigationView.snp.bottom).offset(16)
-            $0.left.right.equalToSuperview().inset(16)
-            $0.height.equalTo(42)
-        }
-        searchImageView.snp.makeConstraints {
-            $0.size.equalTo(20)
-            $0.left.equalTo(searchBarView.snp.left).offset(10)
-            $0.centerY.equalTo(searchBarView)
-        }
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(searchBarView.snp.bottom).offset(24)
-            $0.left.equalToSuperview().inset(16)
-        }
-        removeAllButton.snp.makeConstraints {
-            $0.centerY.equalTo(titleLabel.snp.centerY)
-            $0.right.equalToSuperview().inset(16)
-        }
-        recentTableView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
-            $0.left.bottom.right.equalToSuperview().inset(16)
-        }
-        emptyRecentSearchLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
-        searchResultTableView.snp.makeConstraints {
-            $0.top.equalTo(searchBarView.snp.bottom).offset(8)
-            $0.left.bottom.right.equalToSuperview().inset(16)
-        }
-        
-        performRecentDataSource()
-        performSearchDataSource()
-        performRecentDataSnapShot()
     }
     
     //MARK: - Rx Binding..
@@ -272,29 +199,6 @@ extension SearchBarVC: UITableViewDelegate {
         })
     }
     
-    private func performSearchDataSource() {
-        searchDataSource = UITableViewDiffableDataSource<Section, ResponsePOI>(tableView: searchResultTableView, cellProvider: { [weak self] tableView, indexPath, poi in
-            let cell = tableView.dequeueReusableCell(withType: SearchResultCell.self, for: indexPath)
-            
-            let attrString = NSMutableAttributedString(string: poi.name ?? "")
-            cell.titleLabel.attributedText = attrString.apply(word: self?.searchBarView.contentTextField.text ?? "",
-                                                              attrs: [.foregroundColor: UIColor.black])
-            cell.subTitleLabel.text = poi.address
-            
-            if let departure = LocationManager.shared.currentLocation,
-               let targetCoordinate = poi.coordinate {
-                let destination = CLLocation(latitude: targetCoordinate.latitude, longitude: targetCoordinate.longitude)
-                
-                let distance = departure.distance(from: destination)
-                let distanceString = distance < 1000 ? "\(Int(distance))m" : "\(Double(Int(distance * 10 / 1000)) / 10)km"
-                
-                cell.distanceLabel.text = distanceString
-            }
-            
-            return cell
-        })
-    }
-    
     private func performRecentDataSnapShot() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, ResponsePOI>()
         snapshot.appendSections([.search])
@@ -310,27 +214,7 @@ extension SearchBarVC: UITableViewDelegate {
         self.recentDataSource?.apply(snapshot, animatingDifferences: false)
     }
     
-    private func performSearchDataSnapShot(pois: [ResponsePOI]) {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, ResponsePOI>()
-        snapshot.appendSections([.search])
-        
-        snapshot.appendItems(pois)
-        self.searchDataSource?.apply(snapshot, animatingDifferences: false)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return tableView == recentTableView ? 50 : 86
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView != recentTableView {
-            DataManager.shared.addNew(poi: viewModel.pois[indexPath.row])
-            delegate?.fetch(name: viewModel.pois[indexPath.row].name, coordinate: viewModel.pois[indexPath.row].coordinate)
-        } else {
-            let target = CLLocationCoordinate2D(latitude: DataManager.shared.pois[indexPath.row].latitude,
-                                                longitude: DataManager.shared.pois[indexPath.row].longitude)
-            delegate?.fetch(name: DataManager.shared.pois[indexPath.row].name, coordinate: target)
-        }
         
         navigationController?.popViewController(animated: true)
     }
@@ -344,5 +228,120 @@ extension SearchBarVC: RecentResultCellProtocol {
         DataManager.shared.pois.remove(at: index)
         
         performRecentDataSnapShot()
+    }
+}
+
+extension SearchBarVC: SearchResultViewDelegate {
+    func didTapSearchResult(poi: ResponsePOI) {
+        
+    }
+    
+    private func configureSearchResultView() {
+        searchResultView.delegate = self
+    }
+}
+
+
+//MARK: - Set UI
+private extension SearchBarVC {
+    enum UIConstants {
+        enum NavigationView {
+            static let title: String = "주소 검색"
+            static let height: CGFloat = 47.0
+        }
+        
+        enum SearchImageView {
+            static let image: UIImage = Asset.Images.search.image.withRenderingMode(.alwaysTemplate)
+            
+            static let leftOffset: CGFloat = 10
+            static let size: CGFloat = 20
+        }
+        
+        enum SearchBarView {
+            static let content: String = "주유소 위치를 검색해보세요."
+            static let contentFont: UIFont = FontFamily.NanumSquareRound.regular.font(size: 12)
+            static let titleWidth: CGFloat = 0.1
+            static let contentTrailing: CGFloat = 8
+            
+            static let topOffset: CGFloat = 16
+            static let horizontalInsets: CGFloat = 16
+            static let height: CGFloat = 42
+        }
+        
+        enum TitleLabel {
+            static let text: String = "최근 검색"
+            static let font: UIFont = FontFamily.NanumSquareRound.bold.font(size: 14)
+            
+            static let topOffset: CGFloat = 24
+            static let leftOffset: CGFloat = 16
+        }
+        
+        enum RemoveAllButton {
+            static let title: String = "전체 삭제"
+            static let font: UIFont = FontFamily.NanumSquareRound.regular.font(size: 12)
+            
+            static let rightOffset: CGFloat = -16
+        }
+        
+        enum EmptyRecentSearchLabel {
+            static let text: String = "최근 검색한 내역이 없습니다"
+            static let font: UIFont = FontFamily.NanumSquareRound.bold.font(size: 18)
+        }
+    }
+    
+    func makeUI() {
+        configureUI()
+        setConstraints()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(navigationView)
+        view.addSubview(searchBarView)
+        view.addSubview(searchImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(removeAllButton)
+        view.addSubview(recentTableView)
+        view.addSubview(emptyRecentSearchLabel)
+        view.addSubview(searchResultView)
+    }
+    
+    func setConstraints() {
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(UIConstants.NavigationView.height)
+        }
+        searchBarView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(UIConstants.SearchBarView.topOffset)
+            $0.horizontalEdges.equalToSuperview().inset(UIConstants.SearchBarView.horizontalInsets)
+            $0.height.equalTo(UIConstants.SearchBarView.height)
+        }
+        searchImageView.snp.makeConstraints {
+            $0.left.equalTo(searchBarView.snp.left).offset(UIConstants.SearchImageView.leftOffset)
+            $0.centerY.equalTo(searchBarView)
+            $0.size.equalTo(UIConstants.SearchImageView.size)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(searchBarView.snp.bottom).offset(UIConstants.TitleLabel.topOffset)
+            $0.left.equalToSuperview().inset(UIConstants.TitleLabel.leftOffset)
+        }
+        removeAllButton.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.right.equalToSuperview().offset(UIConstants.RemoveAllButton.rightOffset)
+        }
+        recentTableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.left.bottom.right.equalToSuperview().inset(16)
+        }
+        emptyRecentSearchLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        searchResultView.snp.makeConstraints {
+            $0.top.equalTo(searchBarView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
     }
 }
---
+++ OilPrice-Where/Scenes/Main/Search/Views/Cells/RecentResultCell.swift
@@ -0,0 +1,133 @@
+//
+//  RecentResultCell.swift
+//  OilPrice-Where
+//
+//  Created by wargi on 2022/10/30.
+//  Copyright © 2022 sangwook park. All rights reserved.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+
+protocol RecentResultCellDelegate: AnyObject {
+    func delete(poi: POIEntity?, index: Int?)
+}
+
+
+//MARK: RecentResultCell
+final class RecentResultCell: UICollectionViewCell {
+    //MARK: - Properties
+    weak var delegate: RecentResultCellDelegate?
+    private var poi: ResponsePOI?
+    private var index: Int?
+    
+    private let locationImageView = UIImageView().then {
+        let image = Asset.Images.geoIcon.image.withRenderingMode(.alwaysTemplate)
+        $0.image = image
+        $0.tintColor = .black
+    }
+    private let titleLabel = UILabel().then {
+        $0.font = FontFamily.NanumSquareRound.regular.font(size: 14)
+    }
+    private let dateLabel = UILabel().then {
+        $0.textAlignment = .right
+        $0.font = FontFamily.NanumSquareRound.light.font(size: 14)
+    }
+    private let deleteImageView = UIImageView().then {
+        let image = Asset.Images.close.image.withRenderingMode(.alwaysTemplate)
+        $0.image = image
+        $0.tintColor = .systemGray3
+    }
+    private lazy var deleteButton = UIButton().then {
+        $0.addTarget(self, action: #selector(deleteButtonTouched), for: .touchUpInside)
+    }
+    private let lineView = UIView().then {
+        $0.backgroundColor = .systemGray5
+    }
+    
+    let dateFormatter = DateFormatter().then {
+        $0.dateFormat = "MM/dd"
+        $0.locale = Locale(identifier: "ko_KR")
+    }
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with poi: ResponsePOI, index: Int) {
+        self.poi = DataManager.shared.pois[index]
+        self.index = index
+        titleLabel.text = poi.name
+        dateLabel.text = dateFormatter.string(from: poi.insertDate ?? Date())
+    }
+    
+    @objc
+    private func deleteButtonTouched() {
+        delegate?.delete(poi: poi, index: index)
+    }
+}
+
+
+// MARK: - UI Setup
+private extension RecentResultCell {
+    enum UIConstants {
+        static let locationImageSize: CGFloat = 20
+        static let deleteButtonSize: CGFloat = 38
+        static let deleteImageSize: CGFloat = 10
+        static let interItemSpacing: CGFloat = 8
+        static let lineHeight: CGFloat = 1
+    }
+    
+    func setupUI() {
+        contentView.addSubview(locationImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(deleteImageView)
+        contentView.addSubview(deleteButton)
+        contentView.addSubview(lineView)
+    }
+    
+    func setupConstraints() {
+        contentView.snp.makeConstraints { make in
+            make.height.equalTo(50)
+        }
+        locationImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.size.equalTo(UIConstants.locationImageSize)
+        }
+        deleteButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview()
+            make.size.equalTo(UIConstants.deleteButtonSize)
+        }
+        deleteImageView.snp.makeConstraints { make in
+            make.center.equalTo(deleteButton)
+            make.size.equalTo(UIConstants.deleteImageSize)
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalTo(deleteButton.snp.left)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(locationImageView.snp.right).offset(UIConstants.interItemSpacing)
+            make.right.equalTo(dateLabel.snp.left).offset(-UIConstants.interItemSpacing)
+        }
+        lineView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(UIConstants.lineHeight)
+        }
+    }
+}
